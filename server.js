d5b9c940c5d4e0a170c779f39d4e3a72587b340d
Swayam017 <swayampravasahoo017@gmail.com>
2026-04-23 23:30:14 +0530

    config

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ connectDB()
     //console.log("🔥 Worker started");
 
     // ✅ THEN start server
-    app.listen(5000, () =>
-      console.log("🚀 Server running on port 5000")
-    );
+   const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("❌ DB Connection Failed:", err);
